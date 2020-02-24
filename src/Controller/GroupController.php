1cd0d6f23301b7af49e3e4c4f95b150aebc1bdd3
arguezs <rs.alejandro.93@gmail.com>
2020-02-24 16:34:47 +0100

    Users can now create groups of users.

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Group;
+use App\Form\NewGroupFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +16,30 @@ class GroupController extends AbstractController {
      * @Route("/groups", name="groups")
      * @return RedirectResponse|Response
      */
-    public function groups() {
+    public function groups(Request $request) {
         if (!$this->getUser())
             return $this->redirectToRoute('sign_in');
 
-        return $this->render('groups/groupList.html.twig',
-            [ 'groups' => $this->getUser()->getGroups() ]);
+        $group = new Group();
+        $group->setOwner($this->getUser());
+
+        $form = $this->createForm(NewGroupFormType::class, $group);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($group);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Group created');
+            return $this->redirectToRoute('groups');
+        }
+
+        return $this->render('groups/groupList.html.twig', [
+            'groups' => $this->getUser()->getGroups(),
+            'newGroupForm' => $form->createView()
+        ]);
     }
 
 }
---
+++ src/Form/NewGroupFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewGroupFormType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Group name',
+                    'class' => 'form-control my-3',
+                    'required' => true
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Create group',
+                'attr' => ['class' => 'btn btn-secondary']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefault('data_class', Group::class);
+    }
+
+}
