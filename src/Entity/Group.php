527b1814f64a9a7fa864fef6dc84aca168ee421a
arguezs <rs.alejandro.93@gmail.com>
2020-02-24 16:12:12 +0100

    Fixed bug with Group entity.

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -7,56 +7,86 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\GroupRepository")
+ * Group
+ *
+ * @ORM\Table(name="groups", indexes={@ORM\Index(name="IDX_6DC044C57E3C61F9", columns={"owner_id"})})
+ * @ORM\Entity
  */
-class Group
-{
+class Group {
+
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
      */
     private $name;
 
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="groups")
-     * @ORM\JoinColumn(nullable=false)
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="owner_id", referencedColumnName="id")
+     * })
      */
     private $owner;
 
     /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\User")
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="group")
+     * @ORM\JoinTable(name="group_user",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="group_id", referencedColumnName="id")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *   }
+     * )
      */
-    private $member;
+    private $user;
 
-    public function __construct() {
-        $this->member = new ArrayCollection();
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->user = new ArrayCollection();
     }
 
-    public function getId(): ?int {
+    public function getId(): ?int
+    {
         return $this->id;
     }
 
-    public function getName(): ?string {
+    public function getName(): ?string
+    {
         return $this->name;
     }
 
-    public function setName(string $name): self {
+    public function setName(string $name): self
+    {
         $this->name = $name;
 
         return $this;
     }
 
-    public function getOwner(): ?User {
+    public function getOwner(): ?User
+    {
         return $this->owner;
     }
 
-    public function setOwner(?User $owner): self {
+    public function setOwner(?User $owner): self
+    {
         $this->owner = $owner;
 
         return $this;
@@ -65,23 +95,27 @@ class Group
     /**
      * @return Collection|User[]
      */
-    public function getMembers(): Collection {
-        return $this->member;
+    public function getUser(): Collection
+    {
+        return $this->user;
     }
 
-    public function addMember(User $member): self {
-        if (!$this->member->contains($member)) {
-            $this->member[] = $member;
+    public function addUser(User $user): self
+    {
+        if (!$this->user->contains($user)) {
+            $this->user[] = $user;
         }
 
         return $this;
     }
 
-    public function removeMember(User $member): self {
-        if ($this->member->contains($member)) {
-            $this->member->removeElement($member);
+    public function removeUser(User $user): self
+    {
+        if ($this->user->contains($user)) {
+            $this->user->removeElement($user);
         }
 
         return $this;
     }
+
 }
