7b814043697a738b597892e879ae2c45555562b0
Alejandro <rs.alejandro.93@gmail.com>
2020-02-25 20:25:59 +0100

    Users can now remove messages from their inbox.

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -13,17 +13,61 @@ class MessageController extends AbstractController {
 
     /**
      * @Route("/message/{id}", name="message")
+     * @param $id
      * @return RedirectResponse|Response
      */
     public function message($id){
         if (!$this->getUser())
             return $this->redirectToRoute('home');
 
-        $messageRepo = $this->getDoctrine()->getRepository(Message::class);
-        $message = $messageRepo->find($id);
+        $message = $this->isVisible($id);
 
         if (!$message)
-            return $this->redirectToRoute('error', [ 'errorCode' => 404 ]);
+            return $this->redirectToRoute('error', 404);
+
+        return $this->render('message/message.html.twig', [
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @Route("/message/{messageId}/delete", name="delete_msg")
+     * @param $messageId
+     * @return RedirectResponse
+     */
+    public function deleteMessage($messageId){
+        if (!$this->getUser())
+            return $this->redirectToRoute('sign_in');
+
+        $message = $this->isVisible($messageId);
+
+        if (!$message)
+            return $this->redirectToRoute('error', 404);
+
+        $inbox = $this->getDoctrine()
+            ->getRepository(Inbox::class)
+            ->findBy([
+                'user' => $this->getUser(),
+                'message' => $message
+            ]);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        foreach ($inbox as $link){
+            $entityManager->remove($link);
+            $entityManager->flush();
+        }
+
+        $this->addFlash('success', 'Message was removed');
+        return $this->redirectToRoute('inbox');
+    }
+
+    private function isVisible($messageId){
+        $messageRepo = $this->getDoctrine()->getRepository(Message::class);
+        $message = $messageRepo->find($messageId);
+
+        if (!$message)
+            return false;
 
         $inboxRepo = $this->getDoctrine()->getRepository(Inbox::class);
         $inbox = $inboxRepo->findBy([
@@ -32,7 +76,7 @@ class MessageController extends AbstractController {
         ]);
 
         if (!$inbox)
-            return $this->redirectToRoute('error', [ 'errorCode' => 401 ]);
+            return false;
 
         foreach ($inbox as $row){
             if (!$row->getIsRead()) {
@@ -42,9 +86,7 @@ class MessageController extends AbstractController {
             }
         }
 
-        return $this->render('message/message.html.twig', [
-            'message' => $message
-        ]);
+        return $message;
     }
 
 }
